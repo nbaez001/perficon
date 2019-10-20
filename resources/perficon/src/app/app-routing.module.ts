6a258b58403b3026675726345f653699d2dbe355
nbaez001 <nbaez001@gmail.com>
2019-10-20 16:12:17 -0500

    Configuracion angular multi module

--- resources/perficon/src/app/app-routing.module.ts
+++ resources/perficon/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SesionComponent } from './modules/sesion/sesion.component';
 
-const routes: Routes = [];
+const rootRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sesion',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: SesionComponent,
+    children: [
+      {
+        path: 'sesion',
+        loadChildren: './modules/sesion/sesion.module#SesionModule',
+        data: { title: 'Session' }
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'sesion/404'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(rootRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- resources/perficon/src/app/app.module.ts
+++ resources/perficon/src/app/app.module.ts
@@ -1,8 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { GestureConfig } from '@angular/material';
+import { MaterialModule } from './modules/material.module';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +26,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule, 
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig },
+    { provide: DEFAULT_PERFECT_SCROLLBAR_CONFIG, useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
